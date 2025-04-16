218517a31b3ad0415e2d423f72bf662a83b54a9a
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-16 15:27:47 +0530

    added fields, constructor and getter setter methods to Author.java

--- src/main/java/com/csa/bookstore/model/Author.java
+++ src/main/java/com/csa/bookstore/model/Author.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.csa.bookstore.model;
 
 /**
@@ -9,5 +5,35 @@ package com.csa.bookstore.model;
  * @author user
  */
 public class Author {
-    
+    private String name;
+    private String biography;
+
+    public Author() {
+    }
+
+    public Author(String name, String biography) {
+        this.name = name;
+        this.biography = biography;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "name=" + name + ", biography=" + biography + '}';
+    }
 }
