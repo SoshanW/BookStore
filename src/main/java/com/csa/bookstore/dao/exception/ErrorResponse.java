9ff77e81d9639dc12d37dee351dcb8426a88bb3d
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-16 16:14:25 +0530

    added simple error reponse model for JSON error output

--- src/main/java/com/csa/bookstore/dao/exception/ErrorResponse.java
+++ src/main/java/com/csa/bookstore/dao/exception/ErrorResponse.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.csa.bookstore.dao.exception;
 
 /**
  *
- * @author user
+ * @author Soshan Wijayarathne
  */
-class ErrorResponse {
+public class ErrorResponse {
+    private String code;
+    private String message;
+    
+    public ErrorResponse(){
+    }
+
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
     
 }
