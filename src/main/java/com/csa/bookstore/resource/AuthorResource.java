a023d869c7ffc15bccc4ea41091ffe3bcc7ae405
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-16 19:21:53 +0530

    added method to get books by author

--- src/main/java/com/csa/bookstore/resource/AuthorResource.java
+++ src/main/java/com/csa/bookstore/resource/AuthorResource.java
@@ -1,8 +1,12 @@
 package com.csa.bookstore.resource;
 
 import com.csa.bookstore.dao.AuthorDAO;
+import com.csa.bookstore.dao.BookDAO;
 import com.csa.bookstore.entity.Author;
+import com.csa.bookstore.entity.Book;
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -25,6 +29,8 @@ import javax.ws.rs.core.UriInfo;
 @Consumes(MediaType.APPLICATION_JSON)
 public class AuthorResource {
     private final AuthorDAO authorDAO = new AuthorDAO();
+    private final BookDAO bookDAO = new BookDAO();
+
     
     @POST
     public Response addAuthor(Author author, @Context UriInfo uriInfo){
@@ -55,5 +61,15 @@ public class AuthorResource {
     public Response deleteAuthor(@PathParam("id") int id) {
         authorDAO.deleteAuthor(id);
         return Response.noContent().build();
+    }
+    
+    @GET
+    @Path("/{id}/books")
+    public Response getBooksByAuthor(@PathParam("id") int id) {
+        Author author = authorDAO.getAuthorById(id);
+        List<Book> books = bookDAO.getAllBooks().stream()
+                .filter(book -> book.getAuthor().equalsIgnoreCase(author.getName()))
+                .collect(Collectors.toList());
+        return Response.ok(books).build();
     }
 }
