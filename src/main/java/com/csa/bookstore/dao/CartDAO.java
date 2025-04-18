822c67a1cbcd19f417566e39f51cdb3438ed6a08
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-18 11:51:47 +0530

    completed CartDAO with getCart, updateItem, removeItem and clearCart methods

--- src/main/java/com/csa/bookstore/dao/CartDAO.java
+++ src/main/java/com/csa/bookstore/dao/CartDAO.java
@@ -1,6 +1,7 @@
 package com.csa.bookstore.dao;
 
 import com.csa.bookstore.entity.Cart;
+import com.csa.bookstore.exception.CartNotFoundException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -18,5 +19,34 @@ public class CartDAO {
                 item == null ? new Cart(bookId, quantity) : new Cart(bk, item.getQuantity() + quantity));
             return v;
         });
+    }
+    
+    public Map<String, Cart> getCart(int customerId){
+        Map<String, Cart> cart = carts.get(customerId);
+        if (cart == null) {
+            throw new CartNotFoundException("Cart for customer "+customerId+" not found");
+        }
+        return cart;
+    }
+    
+    public void updateItem(int customerId, String bookId, int quantity){
+        carts.computeIfPresent(customerId, (k, v)->{
+            v.computeIfPresent(bookId, (bk, item)->{
+                item.setQuantity(quantity);
+                return item;
+            });
+            return v;
+        });
+    }
+    
+    public void removeItem(int customerId, String bookId){
+        carts.computeIfPresent(customerId, (k, v)->{
+            v.remove(bookId);
+            return v;
+        });
+    }
+    
+    public void clearCart(int customerId){
+        carts.remove(customerId);
     }
 }
