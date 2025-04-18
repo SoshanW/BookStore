7a052cafe4abfc60b8af1c1d8e0ca61e1e2e15fc
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-18 11:23:51 +0530

    implemented logic to add items to cart

--- src/main/java/com/csa/bookstore/dao/CartDAO.java
+++ src/main/java/com/csa/bookstore/dao/CartDAO.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.csa.bookstore.dao;
+
+import com.csa.bookstore.entity.Cart;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
- * @author user
+ * @author Soshan Wijayarathne
  */
 public class CartDAO {
+    private static final Map<Integer, Map<String, Cart>> carts = new ConcurrentHashMap<>();
     
+    public void addItem(int customerId, String bookId, int quantity){
+        carts.compute(customerId, (k, v) -> {
+            if (v == null) v = new ConcurrentHashMap<>();
+            v.compute(bookId, (bk, item) -> 
+                item == null ? new Cart(bookId, quantity) : new Cart(bk, item.getQuantity() + quantity));
+            return v;
+        });
+    }
 }
